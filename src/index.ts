b3dc259be35266952dac2ebb57fc6245268119c1
fj <menspocky322@gmail.com>
2022-01-31 21:10:23 +0900

    ebookjapanの検索が完成した

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { ebookJapan } from './sites/ebook-japan'
 
 const searchEbookJapan = async (keyword: string) => {
   const { searchTitle } = ebookJapan()
-  await searchTitle(keyword)
+  return await searchTitle(keyword)
 }
 
 const main = async () => {
-  const keyword = 'トリリオンゲーム'
-  await searchEbookJapan(keyword)
+  const keyword = 'メイの大冒険'
+  const ebookJapan = await searchEbookJapan(keyword)
+  console.log('[ebookjapan]', ebookJapan)
 }
 
 main()
--- src/sites/ebook-japan.ts
+++ src/sites/ebook-japan.ts
@@ -4,15 +4,40 @@ export const ebookJapan = () => {
   const URL = 'https://ebookjapan.yahoo.co.jp/free/'
   const SEARCH_BOX_SELECTOR =
     '#wrapper > header > div.header__contents.header-contents > div.search-form.header-contents__search > div > div.search-box > form > input'
+  const FIND_ITEM_SELECTOR =
+    '#wrapper > div.contents-wrapper > div.main > div > div > div > div > div.contents-search > div > ul > li:nth-child(1) > div > a'
+  const TITLE_SELECTOR =
+    '#wrapper > div.contents-wrapper > div.main > div > div.page-series > div > div.contents-main > div.book-main > div:nth-child(1) > div > h1'
+  const SEARCH_ZERO =
+    '#wrapper > div.contents-wrapper > div.main > div > div > div > div > div.search-zero > div'
   const searchTitle = async (keyword: string) => {
     const browser = await playwright.launchChromium({
       headless: false,
     })
-    const context = await browser.newContext()
-    const page = await context.newPage()
-    await page.goto(URL)
-    await page.fill(SEARCH_BOX_SELECTOR, keyword)
-    await page.keyboard.press('Enter')
+    try {
+      const context = await browser.newContext()
+      const page = await context.newPage()
+      await page.goto(URL)
+      await page.fill(SEARCH_BOX_SELECTOR, keyword)
+      await page.keyboard.press('Enter')
+      await page.waitForTimeout(3000)
+      if (await page.locator(SEARCH_ZERO).isVisible()) {
+        await browser.close()
+        console.log('[ebookJapan]ご指定の条件に該当する作品はありませんでした。')
+        return false
+      }
+      await page.click(FIND_ITEM_SELECTOR)
+      const title = await page.locator(TITLE_SELECTOR).textContent()
+
+      const hasTitle = title?.includes(keyword)
+      await browser.close()
+      return hasTitle
+    } catch (error: any) {
+      console.log('[ebookjapan] 例外が発生しました。', error)
+      if (browser) {
+        await browser.close()
+      }
+    }
   }
 
   return { searchTitle }
